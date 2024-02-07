380fdb226fa5d6b090db0616898cb7bf9f498835
Luis Espana <fdh.luis.angel@gmail.com>
2024-02-07 11:29:02 -0500

    :sparkles: New feature :: Dataset splitter

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import librosa as lb
 
 def pad_trunc_audio(audio, sampling_freq, max_dur):
     '''
@@ -64,3 +65,26 @@ def tensor_to_nparray(tensor):
     np_array = np.squeeze(np_array)
     
     return np_array
+
+
+def train_test_val_split(dataset, percent, train_percent, val_percent, test_percent):
+    '''
+    Función para dividir el conjunto de datos en entrenamiento, validación y evaluación.
+    
+    Parámetros:
+    - dataset: Conjunto de entrada. Tensor de TensorFlow
+    - training_size: Tamaño del conjunto de entrenamiento deseado.
+    - validation_size: Tamaño del conjunto de validación deseado.
+    (El tamaño del conjunto de evaluación se calcula automaticamente).
+    Returns:
+    - train_set: Conjunto de entrenamiento.
+    - val_set: Conjunto de validación.
+    - test_set: Conjunto de evaluación.
+    '''
+    size = dataset.cardinality().numpy() * percent
+
+    train_size = tf.math.rint(tf.math.multiply(size, tf.cast(train_percent, tf.float32)))
+    val_size = tf.math.rint(tf.math.multiply(size, tf.cast(val_percent, tf.float32)))
+    test_size = tf.math.rint(tf.math.multiply(size, tf.cast(test_percent, tf.float32)))
+    
+    return tf.cast(train_size, tf.int64), tf.cast(val_size, tf.int64), tf.cast(test_size, tf.int64)
